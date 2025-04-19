18e064f25100cb4a877ffaf2edc0d1b149977bc3
spyder5658 <thesecretengineers007@gmail.com>
2025-04-19 23:04:54 +0545

    Update:Confogure pin A6 as input

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -22,38 +22,19 @@
 #include "main.h"
 
 /* Private includes ----------------------------------------------------------*/
-/* USER CODE BEGIN Includes */
-
-/*   Understand the below addresses and their significance by reading the blog title know the architecture*/
 
 
-// #define PERIPHERAL_BASE             (0x40000000UL)
-// #define APB2PHERIPHERAL_OFFSET      (0x10000UL)
-// #define APB2PERIPHERAL_BASE          (PERIPHERAL_BASE + APB2PHERIPHERAL_OFFSET)
-
-// #define GPIOC_OFFSET                 (0x1000UL)
-// #define GPIOC_BASE         (APB2PERIPH_BASE + GPIOC_OFFSET)
-
-// #define RCC_OFFSET         (0x11000UL)
-// #define RCC_BASE           (APB2PERIPH_BASE + RCC_OFFSET)
-
-// #define APB2EN_R_OFFSET    (0x18UL)
-// #define RCC_APB2EN_R       (*(volatile unsigned int *)(RCC_BASE + APB2EN_R_OFFSET))
-
-// #define CRH_R_OFFSET       (0x04UL)
-// #define GPIOC_CRH_R        (*(volatile unsigned int *)(GPIOC_BASE + CRH_R_OFFSET))
-
-// #define OD_R_OFFSET        (0x0CUL)
-// #define GPIOC_OD_R         (*(volatile unsigned int *)(GPIOC_BASE + OD_R_OFFSET))
-
-/*------------------------------------------------------------------------------------*/
-
-
-#define GPIOC_EN           (1U<<4)
-
-#define PIN13              (1U<<13)
+#define GPIOC_EN           (1U<<4)    
+                                      
+#define PIN13              (1U<<13)   
 #define LED_PIN             PIN13
 #define LED_OFF             (1U<<29)
+  
+
+#define GPIOA_EN           (1U<<2)
+#define INPUT_PIN           (1U<<6)
+
+
 
 
 /* USER CODE END Includes */
@@ -77,7 +58,18 @@ int main(void)
   GPIOC->CRH |= (1U<<20);
   GPIOC->CRH |= (1U<<21);
   GPIOC->CRH &= ~(1U<<22);
-  GPIOC->CRH &= ~(0U<<23);
+  GPIOC->CRH &= ~(1U<<23);
+
+
+
+  RCC->APB2ENR |=GPIOA_EN;
+
+  GPIOA->CRH &= ~(1U<<24);
+  GPIOA->CRH &= ~(1U<<25);
+  GPIOA->CRH &= ~(1U<<26);
+  GPIOA->CRH &= ~(1U<<24);
+
+
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -85,17 +77,16 @@ int main(void)
   while (1)
   {
 
-    /*----------- X-OR way to blink----------*/
-    // GPIOC->ODR ^= LED_PIN;                      //uncomment to 
-    // for(int i=0;i<100000;i++);                  //use the code fo X-OR; think how it does the blinking
-    /*----------- X-OR way to blink----------*/
-
-
-    GPIOC->BSRR = LED_PIN;
-		for(int i=0; i<100000; i++){}
-
-		GPIOC->BSRR = LED_OFF;
-		for(int i=0; i<100000; i++){}
+    if(GPIOA->IDR & INPUT_PIN)
+    {
+      GPIOC->ODR &= ~LED_PIN;
+      // GPIOC->BSRR = LED_OFF;
+    }
+    else
+    {
+      // GPIOC->BSRR = LED_PIN;
+      GPIOC->ODR |=LED_PIN;
+    }
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
